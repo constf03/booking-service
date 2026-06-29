d3738a61a55cef08580273956f9ffa81de020cc1
constf03 <stefano.confalone03@gmail.com>
2026-06-29 18:36:50 +0300

    update create-booking.php

--- src/create-booking.php
+++ src/create-booking.php
@@ -6,23 +6,23 @@ try {
     // Convert request data to JSON
     $data = json_decode(file_get_contents('php://input'), true);
 
-    $req_user_id        =   $data["user_id"];
-    $req_slot_number    =   $data["slot_number"];
-    $req_week_number    =   $data["week_number"];
-    $req_date           =   $data["date"];
-    $req_time           =   $data["time"];
+    $req_user_id = $data["user_id"];
+    $req_slot_number = $data["slot_number"];
+    $req_week_number = $data["week_number"];
+    $req_date = $data["date"];
+    $req_time = $data["time"];
 
     if (
-        empty($req_user_id) &&
-        empty($req_slot_number) &&
-        empty($req_week_number) &&
-        empty($req_date) &&
+        empty($req_user_id) ||
+        empty($req_slot_number) ||
+        empty($req_week_number) ||
+        empty($req_date) ||
         empty($req_time)) {
         exit("Error: Missing data.");
     }
 
     $query = "INSERT INTO bookings (user_id, slot_number, week_number, date, time)
-        VALUES (?, ?, ?, ?, ?)";
+              VALUES (?, ?, ?, ?, ?)";
 
     // Prepare SQL statement (prevent SQL injection)
     $stmt = $mysqli->stmt_init();
@@ -42,7 +42,7 @@ try {
 
     if ($stmt->execute()) {
         $mysqli->close();
-        header("Location: ../public/index.php");
+        header("Location: index.php");
         exit;
     } else {
         exit("SQL Error: " . $mysqli->error . $mysqli->errno);
